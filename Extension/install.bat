81b7e5baf3de4c680c2d1e8d11653e835709324e
Thomas Fischer <tfischer@beamng.com>
2018-03-02 16:58:39 +0100

    using link for installing now

--- Extension/install.bat
+++ Extension/install.bat
@@ -1,2 +1,7 @@
-copy /y *.* %USERPROFILE%\.vscode\extensions\BeamNG.luajit-debug-0.1.0
+:: run this file as administrator to create a folder link in the VSCode extension folder
+
+mklink /j %USERPROFILE%\.vscode\extensions\BeamNG.luajit-debug-0.1.0 %~dp0
+
+:: the copy version of it:
+::copy /y *.* %USERPROFILE%\.vscode\extensions\BeamNG.luajit-debug-0.1.0
 @pause
