935cd60c2a34f2a79bbd30cde1dfaf37f6eb90f1
Thomas Fischer <tfischer@beamng.com>
2018-03-02 16:04:02 +0100

    improved paths

--- Extension/install.bat
+++ Extension/install.bat
@@ -1,2 +1,2 @@
-copy /y *.dll c:\users\rica\.vscode\extensions\devCAT.lua-debug-1.0.8
-copy /y *.exe c:\users\rica\.vscode\extensions\devCAT.lua-debug-1.0.8
+copy /y *.dll %HOMEDRIVE%%HOMEPATH%\.vscode\extensions\devCAT.lua-debug-1.0.8
+copy /y *.exe %HOMEDRIVE%%HOMEPATH%\.vscode\extensions\devCAT.lua-debug-1.0.8
